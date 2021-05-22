2a5916833304fdc8bf74482b9626cccde83f6b02
Gabi Wanabi <gabi.wanabi@gmx.net>
2021-05-22 08:52:08 +0200

    Initial commit

---
+++ src/main/java/com/haegisoft/springbootrestservice/Controller.java
@@ -0,0 +1,14 @@
+package com.haegisoft.springbootrestservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello-world")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
+}
